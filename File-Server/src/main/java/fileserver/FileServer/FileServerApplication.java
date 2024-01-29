f92944ab6905144e4f9c110a0c35e6116d0e2965
Mohammed Ahmed <mohde57@live.com>
2024-01-29 21:26:54 +0000

    Generated project using Spring Initializr

---
+++ File-Server/src/main/java/fileserver/FileServer/FileServerApplication.java
@@ -0,0 +1,13 @@
+package fileserver.FileServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileServerApplication.class, args);
+	}
+
+}
---
+++ File-Server/src/test/java/fileserver/FileServer/FileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fileserver.FileServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
