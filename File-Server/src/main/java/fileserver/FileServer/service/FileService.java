56fa0436088597dd411655e14c4ce47c7ef9c954
Mohammed Ahmed <mohde57@live.com>
2024-01-31 00:29:22 +0000

    Changed the return type for load method from Resource to UrlResource

--- File-Server/src/main/java/fileserver/FileServer/service/FileService.java
+++ File-Server/src/main/java/fileserver/FileServer/service/FileService.java
@@ -1,6 +1,7 @@
 package fileserver.FileServer.service;
 
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 import java.nio.file.Path;
 import java.util.stream.Stream;
@@ -8,7 +9,7 @@ import java.util.stream.Stream;
 public interface FileService {
     void init();
     void save(MultipartFile file);
-    Resource load(String filename);
+    UrlResource load(String filename);
     Stream<Path> loadAll();  
 }
 // FileService
--- File-Server/src/main/java/fileserver/FileServer/service/impl/FileServiceImpl.java
+++ File-Server/src/main/java/fileserver/FileServer/service/impl/FileServiceImpl.java
@@ -1,53 +1,86 @@
 package fileserver.FileServer.service.impl;
 
+import fileserver.FileServer.model.File;
+import fileserver.FileServer.repository.FileRepository;
 import fileserver.FileServer.service.FileService;
 import org.springframework.stereotype.Service;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
-import java.nio.file.Path;
-import java.util.stream.Stream;
 
+import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.*;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
+
+    private final FileRepository fileRepository;
+    private final Path root = Paths.get("uploads");
 
     @Override
     public void init() {
-        // TODO
         try {
+            Files.createDirectories(root);
             System.out.println("FileService initialized!");
-        } catch (Exception e) {
+        } catch (IOException  e) {
             System.err.println("Could not initialize FileService!");
+            throw new RuntimeException("Could not initialize folder for upload!", e);
         }
     }
 
     @Override
     public void save(MultipartFile file) {
-        // TODO
         try {
+            Files.copy(file.getInputStream(), this.root.resolve(file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+            
+            // Save file metadata in the database
+            File fileEntity = new File();
+            fileEntity.setName(file.getOriginalFilename());
+            fileEntity.setUrl(this.root.resolve(file.getOriginalFilename()).toString());
+            fileRepository.save(fileEntity);
             System.out.println("File saved!");
         } catch (Exception e) {
-            System.err.println("Could not save file!");
+            log.error("Could not store the file. Error: ", e);
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage(), e);
         }
     }
 
     @Override
-    public Resource load(String filename) {
-        // TODO
+    public UrlResource load(String filename) {
+        Boolean success = false;
         try {
+            Path file = root.resolve(filename);
+            UrlResource resource = new UrlResource(file.toUri());
             System.out.println("File loaded!");
-        } catch (Exception e) {
+            return resource;
+
+            // if (!resource.exists() || !resource.isReadable()) {
+            //     log.error("Could not load, file does not exist!");
+            //     throw new RuntimeException("Could not load, file does not exist!");
+            // } else {
+            //     success = true;
+            // }
+            // System.out.println("File loaded!");
+            // return resource;
+        } catch (MalformedURLException  e) {
             System.err.println("Could not load file!");
         }
     }
 
     @Override
     public Stream<Path> loadAll() {
-        // TODO
         try {
             System.out.println("All files loaded!");
-        } catch (Exception e) {
-            System.err.println("Could not load all files!");
+            return Files.walk(this.root, 1).filter(path -> !path.equals(this.root)).map(this.root::relativize);
+        } catch (IOException  e) {
+            log.error("Could not load all files!", e);
+            throw new RuntimeException("Could not load the files!", e);
         }
     }
 }
